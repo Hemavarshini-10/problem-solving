f9b181b213b6d297ffd4115c660ac7e1a41f901f
Hemavarshini Jawahar <hemavarshini1036@gmail.com>
2025-02-28 21:12:54 +0530

    Update basics.py
    Write a program which takes two values x and y. Prints x for y number of times.

--- basics.py
+++ basics.py
@@ -1,1 +1,5 @@
-
+# Write a program which takes two values x and y. Prints x for y number of times.
+x=int(input("enter the number:"))
+y=int(input("enter the number:"))
+for _ in range (y):
+  print(x)
