e71f88772e7fa00d30f056b2317bdcd94ce47c6d
Hemavarshini Jawahar <hemavarshini1036@gmail.com>
2025-02-28 21:18:03 +0530

    Create for.py
    Write a program to get firstName and lastName and n as input and print fullName that is firstName+lastName for n times.

---
+++ for.py
@@ -0,0 +1,6 @@
+first_name=input("enter the first name:")
+second_name=input("enter the second name:")
+n=int(input("enter number:"))
+name=first_name+second_name
+for i in range (1,n+1):
+print(name)
